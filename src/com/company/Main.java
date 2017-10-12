a904117355174472553bdddb99c727497c050e96
Fullbusters <32035542+Fullbusters@users.noreply.github.com>
2017-10-12 23:30:21 +0300

    Проста програма hello world

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String a="Hello World!";
+        hello b=new hello();
+        System.out.println("Простий: Hello World!");
+        hello.hello(a);
+        b.hello();
+    }
+}
---
+++ src/com/company/hello.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class hello {
+    static void hello() {
+        System.out.println("Конструктор статік: Hello World!");
+    }
+    static void hello(String a) {
+        System.out.println("Конструктор: "+a);
+    }
+}
